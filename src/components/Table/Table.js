77c4b712f5dd5cc597aeb2edf6d0c8628cca7bb0
fimfei <fishervi@yandex.ru>
2024-10-30 14:16:26 +0700

    Table

---
+++ src/components/Table/Table.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Table = data => {
+
+    return (
+        <div className="unitable-wrapper" >
+            T A B L E ! ! !
+        </div>
+    )
+}
+
+export default Table;
