fe10803fa16591ae3cff096a4d52b95980ea5b71
fimfei <50619572+fimfei@users.noreply.github.com>
2024-10-30 12:30:06 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function Table(data) {
+    console.log(`<Table> from GitHub`);
+}
+
+function callTable(data) {
+    console.log(`callTable from GitHub`);
+}
+
+module.exports = {
+	Table,
+	callTable
+}
