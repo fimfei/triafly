fdcf3444045da363c6d4afb623eaa84b5e4f529a
fimfei <fishervi@yandex.ru>
2024-10-31 09:50:54 +0700

    Table

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-//import Table from './src/components/Table/Table';
+import Table from './src/components/Table/Table';
 
-function Table(data) {
-    console.log(`Table from GitHub`);
-}
+//function Table(data) {
+//    console.log(`Table from GitHub`);
+//}
 
 function callTable(data) {
     console.log(`callTable from GitHub`);
