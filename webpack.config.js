bd09514f334399fd83754b4649d134d0c7cb3470
fimfei <fishervi@yandex.ru>
2024-10-31 12:04:05 +0700

    Table

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,20 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.(js|jsx|ts|tsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
                 }
-            }
+            },
+            {
+                test: /\.js$/,
+                enforce: 'pre',
+                use: ['source-map-loader'],
+            },
         ]
     },
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
     }
 };
