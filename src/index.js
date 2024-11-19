a151d8ace9c180bbb18d5886daa13c07722667cb
fimfei <fishervi@yandex.ru>
2024-11-19 09:30:04 +0700

    'Table'

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {Table, callTable} from './App/components/Table';
+import {Graph} from './App/components/Graph';
+
+import {Popup, callPopup} from './App/widgets/Popup';
+import {Alert, callAlert} from './App/widgets/Alert';
+//import {Prompt, callPrompt} from './App/widgets/Prompt';
+//import {ColorPicker} from './App/widgets/ColorPicker';
+//import {DatePicker} from './App/widgets/DatePicker';
+//import {ElementsPicker} from './App/widgets/ElementsPicker';
+//import {FilesPicker} from './App/widgets/FilesPicker';
+//import {ListPicker} from './App/widgets/ListPicker';
+//import {PeriodPicker} from './App/widgets/PeriodPicker';
+
+//import {InputBool, InputNumber, InputText, SelectFromList} from './App/forms';
+
+import './styles/app.scss';
+
+export {
+    Table, callTable, Graph,
+    Popup, callPopup,
+    Alert, callAlert,
+};
