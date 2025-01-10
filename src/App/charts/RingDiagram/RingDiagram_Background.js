af49328ba78052aa6705a42410a1752dc1ee40c2
fimfei <fishervi@yandex.ru>
2025-01-10 11:58:38 +0700

    'Table'

--- src/App/charts/RingDiagram/RingDiagram_Background.js
+++ src/App/charts/RingDiagram/RingDiagram_Background.js
@@ -5,17 +5,19 @@ const RingDiagramBackground = props => {
     const {diagramSize} = connector.options;
 
     return (
-        React.createElement('rect', {
-            className: `ring-diagram-background${className ? ' ' + className : ''}`,
-            x: 0,
-            y: 0,
-            width: diagramSize,
-            height: diagramSize,
-            fill: externalFill || 'none',
-            stroke: externalStroke || '#888',
-            strokeWidth: externalStrokeWidth || 1,
-            style,
-        })
+        <svg>
+            <rect
+                className={`ring-diagram-background${className ? ' ' + className : ''}`}
+                x="0"
+                y="0"
+                width={diagramSize}
+                height={diagramSize}
+                fill={externalFill || 'none'}
+                stroke={externalStroke || '#888'}
+                strokeWidth={externalStrokeWidth || 1}
+                style={style}
+            />
+        </svg>
     )
 }
 
